5d25d1bc501428851eceff4900f12f0a26539ade
smajumdarNY <smajumdar.itn@gmail.com>
2020-05-19 12:12:57 -0400

    modified app.js/list.js

--- app.js
+++ app.js
@@ -3,46 +3,34 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+let items = ['Buy Food', 'Cook Food'];
+
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.static('public'))
+
 app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
 	let today = new Date();
-	let currentDay = today.getDay();
-	let day = '';
+	// let currentDay = today.getDay();
+	
 
-	// if (currentDay === 6 || currentDay === 0) {
-	// 	day = 'Weekend';
-	// } else {
-	// 	day = 'Weekady';
-	// }
-	switch (currentDay) {
-		case 0:
-			day = 'Sunday';
-			break;
-		case 1:
-			day = 'Monday';
-			break;
-		case 2:
-			day = 'Tuesday';
-			break;
-		case 3:
-			day = 'Wednesday';
-			break;
-		case 4:
-			day = 'Thursday';
-			break;
-		case 5:
-			day = 'Friday';
-			break;
-		case 6:
-			day = 'Saturday';
-			break;
-		default:
-            console.log('Error: Current day is' + currentDay)
-	}
+let options = {
+	weekday : 'long',
+	day : 'numeric',
+	month : 'long'
+}
+	let day = today.toLocaleDateString('en-us', options)
 
-	res.render('list', { kindOfDay: day });
+	res.render('list', { kindOfDay: day, newListItems: items
+	 });
 });
+
+app.post('/', function(req, res){
+	let item = req.body.newItem
+	items.push(item)
+	res.redirect('/')
+})
 
 app.listen(3000, function () {
 	console.log('Server started on port 3000');
